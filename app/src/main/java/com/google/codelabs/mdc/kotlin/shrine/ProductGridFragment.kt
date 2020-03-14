3e0bde8d4a5d3d99f6c64e41085492c79a887347
Navjot Singh <Nav_Singh@manulife.com>
2020-03-14 17:32:00 -0400

    MDC-104 donegit add .

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -1,8 +1,11 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
+import android.os.Build
 import android.os.Bundle
 import android.view.*
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,15 +25,33 @@ class ProductGridFragment : Fragment() {
         // Set up the toolbar.
         val view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
         (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
+        activity?.let {
+            view.app_bar.setNavigationOnClickListener(NavigationIconClickListener(
+                    it,
+                    view.product_grid,
+                    AccelerateDecelerateInterpolator(),
+                    context?.let { ctx -> ContextCompat.getDrawable(ctx, R.drawable.shr_branded_menu) }, // Menu open icon
+                    context?.let { ctx -> ContextCompat.getDrawable(ctx, R.drawable.shr_close_menu) })) // Menu close icon
+
+        }
+
         // Set up the RecyclerView
-        view.recycler_view.setHasFixedSize(true)
-        view.recycler_view.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
-        val adapter = ProductCardRecyclerViewAdapter(
-                ProductEntry.initProductEntryList(resources))
-        view.recycler_view.adapter = adapter
-        val largePadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing)
-        val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
-        view.recycler_view.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
+        with(view.recycler_view) {
+            setHasFixedSize(true)
+            layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+            val adapterItems = ProductCardRecyclerViewAdapter(
+                    ProductEntry.initProductEntryList(resources))
+            adapter = adapterItems
+        }
+        with(resources) {
+            val largePadding = getDimensionPixelSize(R.dimen.shr_product_grid_spacing)
+            val smallPadding = getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
+            view.recycler_view.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
+        }
+        // Set cut corner background for API 23+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            view.product_grid.background = context?.getDrawable(R.drawable.shr_product_grid_background_shape)
+        }
 
         return view
     }
